e507f23e71ff133016fd6ccca0bbbcd1a4d8c084
miguet09 <ma.timbianoc@uea.edu.ec>
2024-08-25 21:42:05 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ 2SEM_SISTEMA_GESTION_SEMANA10/product.py
@@ -0,0 +1,30 @@
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def get_id_producto(self):
+        return self.id_producto
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_cantidad(self):
+        return self.cantidad
+
+    def get_precio(self):
+        return self.precio
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+    def set_cantidad(self, cantidad):
+        self.cantidad = cantidad
+
+    def set_precio(self, precio):
+        self.precio = precio
+
+    def __str__(self):
+        return f'ID: {self.id_producto}, Nombre: {self.nombre}, Cantidad: {self.cantidad}, Precio: ${self.precio:.2f}'
